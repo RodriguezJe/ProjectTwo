a270dcb2337eca024dd602af4ef17fa3da9bd21e
Stephen <stephen.stengel@cwu.edu>
2019-05-08 01:20:03 -0700

    Updated .gitignore

---
+++ MarvinThread.java
@@ -0,0 +1,28 @@
+/*
+ * MarvinThread.java
+ * 
+ * Gives marvin more abilities.
+ * 
+ */
+
+
+public class MarvinThread extends PlayerThread {
+	
+    //~ String threadName;
+
+
+    public MarvinThread(String name) {
+        super(name);
+    }
+	
+    private void freezeOtherPlayer() {
+        
+    }
+    
+    //used when marvin steps on another player's square.
+    private void killOtherPlayer() {
+        //take carrot if they have one
+        //kill other player thread.
+    }
+}
+
